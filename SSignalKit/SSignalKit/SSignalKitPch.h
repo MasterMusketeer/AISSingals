70c72e4f82c79b9de6b3aae6fdff7ddbeb687887
Master <>
2018-06-19 18:03:53 +0700

    commit init

---
+++ SSignalKit/SSignalKit/SSignalKitPch.h
@@ -0,0 +1,19 @@
+//
+//  SSignalKit.h
+//  SSignalKit
+//
+//  Created by aizexin on 2018/6/14.
+//  Copyright © 2018年 master. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSignalKit.
+FOUNDATION_EXPORT double SSignalKitVersionNumber;
+
+//! Project version string for SSignalKit.
+FOUNDATION_EXPORT const unsigned char SSignalKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSignalKit/PublicHeader.h>
+#import <SSignalKit/SSignalKit.h>
+
